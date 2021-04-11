d6d12bcbf5103bd7e605f83ad391521ddd294ced
TimoLempers <81235756+TimoLempers@users.noreply.github.com>
2021-04-11 23:10:07 +0200

    Add files via upload

--- VideostabilizeTest.m
+++ VideostabilizeTest.m
@@ -1,28 +1,16 @@
-%% Video Stabilization
-% This example shows how to remove the effect of camera motion from a video stream.
-
-%   Copyright 2006-2014 The MathWorks, Inc.
+%Image processing Project Man Over Board Group 11
+%Code for stabilizing original video
+%Lisanne Helmer, Timo Lempers, Indra Kumar
 close all
 clear variables
-%% Introduction
-% In this example we first define the target to track. In this case, it is the
-% back of a car and the license plate. We also establish a dynamic search
-% region, whose position is determined by the last known target location.
-% We then search for the target only within this search region, which
-% reduces the number of computations required to find the target. In each
-% subsequent video frame, we determine how much the target has moved
-% relative to the previous frame. We use this information to remove
-% unwanted translational camera motions and generate a stabilized video. 
+ 
 
 %% Initialization
-% Create a System object(TM) to read video from a multimedia file. We set the
-% output to be of intensity only video.
+filename = 'MAH01462.mp4'; %Initializes the original video
 
-% Input video file which needs to be stabilized.
-filename = 'MAH01462.mp4';
-
-hVideoSource = VideoReader(filename);
-a=VideoWriter('C:\Timo Lempers\Masters\Image Processing\Project\project 2\stabilized2.avi');
+hVideoSource = VideoReader(filename); %Reads the original video
+ %Initializes where to save stabilized video
+a=VideoWriter('C:\Timo Lempers\Masters\Image Processing\Project\project 2\stabilized7.avi');
 %%
 open(a);
 
@@ -34,22 +22,20 @@ hTM = vision.TemplateMatcher('ROIInputPort', true, ...
                             'BestMatchNeighborhoodOutputPort', true);
                         
 %%
-% Create a System object to display the original video and the stabilized
-% video.
+%Displaying the original and stabilized video on a single screen
 hVideoOut = vision.VideoPlayer('Name', 'Video Stabilization');
 hVideoOut.Position(1) = round(0.4*hVideoOut.Position(1));
 hVideoOut.Position(2) = round(1.5*(hVideoOut.Position(2)));
 hVideoOut.Position(3:4) = [1650 350];
 
 %%
-% Here we initialize some variables used in the processing loop.
-pos.template_orig = [1000 155]; % [x y] upper left corner
-pos.template_size = [22 18];   % [width height]
-pos.search_border = [15 10];   % max horizontal and vertical displacement
+pos.template_orig = [1000 155]; %Position of object used for stabilization
+pos.template_size = [22 18];   %Size of area of object tracked
+pos.search_border = [15 10];   % Amount of horizontal and vertical movement
 pos.template_center = floor((pos.template_size-1)/2);
 pos.template_center_pos = (pos.template_orig + pos.template_center - 1);
-W = hVideoSource.Width; % Width in pixels
-H = hVideoSource.Height; % Height in pixels
+W = hVideoSource.Width; %Video Width
+H = hVideoSource.Height; % Video Height
 BorderCols = [1:pos.search_border(1)+4 W-pos.search_border(1)+4:W];
 BorderRows = [1:pos.search_border(2)+4 H-pos.search_border(2)+4:H];
 sz = [W, H];
@@ -63,15 +49,12 @@ Target = zeros(1,2);
 firstTime = true;
 
 %% Stream Processing Loop
-% This is the main processing loop which uses the objects we instantiated
-% above to stabilize the input video.
-for t=1:433
-while hasFrame(hVideoSource)
-    input = rgb2gray(im2double(readFrame(hVideoSource)));
-    %input = im2double(readFrame(hVideoSource));
+for t=1:433 %Loops for every frame of the video
+while hasFrame(hVideoSource) %While there are still frames
+    input = rgb2gray(im2double(readFrame(hVideoSource))); %Convert to gray video from RGB
 %%
     % Find location of Target in the input video frame
-    if firstTime
+    if firstTime %Finds the object used to stabilize
       Idx = int32(pos.template_center_pos);
       MotionVector = [100 100];
       firstTime = false;
@@ -87,7 +70,6 @@ while hasFrame(hVideoSource)
     [Offset, SearchRegion] = updatesearch(sz, MotionVector, ...
         SearchRegion, Offset, pos);
 
-    % Translate video frame to offset the camera motion
     Stabilized = imtranslate(input, Offset, 'linear');
    
     Target = Stabilized(TargetRowIndices, TargetColIndices);
@@ -99,20 +81,15 @@ while hasFrame(hVideoSource)
     TargetRect = [pos.template_orig-Offset, pos.template_size];
     SearchRegionRect = [SearchRegion, pos.template_size + 2*pos.search_border];
 
-    % Draw rectangles on input to show target and search region
+    %Draws rectangle on object used for stabilizing
     input = insertShape(input, 'Rectangle', [TargetRect; SearchRegionRect],...
                         'Color', 'white');
-    % Display the offset (displacement) values on the input image
-    txt = sprintf('(%+05.1f,%+05.1f)', Offset);
-    input = insertText(input(:,:,1),[191 215],txt,'FontSize',16, ...
-                    'TextColor', 'white', 'BoxOpacity', 0);
-    % Display video
+
+  
 %%
-    hVideoOut([input(:,:,2) Stabilized]);
-writeVideo(a,Stabilized);
+    hVideoOut([input(:,:,2) Stabilized]); %Display's video 
+writeVideo(a,Stabilized); %Writes stabilized video to video file
 end
 end
-
-
  %%
  close(a);
